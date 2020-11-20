4cb07a25ce7cd0b50628842732baae01e57b30d0
swiftlyfalling <swiftlyfalling@users.noreply.github.com>
2020-11-20 09:56:55 -0500

    Add GitHub Actions CI (#5)

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -6,8 +6,7 @@
 set -e # Exit with nonzero exit code if anything fails
 
 # Requires the following environment variables to be set:
-#   DEPLOY_BRANCH =""          # The only branch from which Travis should deploy
-#   DEPLOY_BASE64_KEY_VAR=""   # The Travis-CI environment variable name that contains the base64-encoded ssh deploy (private) key
+#   DEPLOY_BASE64_KEY_VAR=""   # The environment variable name that contains the base64-encoded ssh deploy (private) key
 #   DEPLOY_REPO_SLUG=""        # The GitHub repo slug (user/repo) for the target (deployment) repo
 #   DEPLOY_TARGET_BRANCH=""    # The target (deployment) repo branch
 #   DEPLOY_GIT_NAME=""         # The name to be used for Git deployment commits
@@ -15,21 +14,6 @@ set -e # Exit with nonzero exit code if anything fails
 
 # Requires the following script input parameters:
 #   $1 = the $SOURCE_DIR (i.e. the directory into which buildrepo.py output)
-
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-	echo "Skipping deployment from pull request."
-	exit
-fi
-
-if [ -z "$DEPLOY_BRANCH" ]; then
-	echo "Skipping deployment; DEPLOY_BRANCH is not set."
-	exit
-fi
-
-if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
-	echo "Skipping deployment; TRAVIS_BRANCH ('$TRAVIS_BRANCH') does not match DEPLOY_BRANCH ('$DEPLOY_BRANCH')."
-	exit
-fi
 
 # Check if other required environment variables are set
 if [ -z "$DEPLOY_BASE64_KEY_VAR" ]; then
@@ -69,18 +53,22 @@ fi
 
 echo "Set up SSH"
 
+if [ ! -d "$HOME/.ssh/" ]; then
+    mkdir -p "$HOME/.ssh/"
+fi
+
 # IMPORTANT: Turn off command traces while dealing with the private key
 set +x
 
 # Get the encrypted private key from the repo settings
-echo ${!DEPLOY_BASE64_KEY_VAR} | base64 --decode > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
+echo ${!DEPLOY_BASE64_KEY_VAR} | base64 --decode > $HOME/.ssh/id_rsa
+chmod 600 $HOME/.ssh/id_rsa
 
 # IMPORTANT: Anyone can read the build log, so it MUST NOT contain any sensitive data
 # set -x # For now, do not re-enable
 
 # Add GitHub's public key
-echo "|1|qPmmP7LVZ7Qbpk7AylmkfR0FApQ=|WUy1WS3F4qcr3R5Sc728778goPw= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> ~/.ssh/known_hosts
+echo "|1|qPmmP7LVZ7Qbpk7AylmkfR0FApQ=|WUy1WS3F4qcr3R5Sc728778goPw= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> $HOME/.ssh/known_hosts
 
 
 # Create a temporary directory in which to clone the destination repo branch
@@ -97,18 +85,18 @@ rsync -rt --delete --exclude=".git" "$SOURCE_DIR/" "$TARGET_DIR/"
 
 # Build the commit message
 COMMIT_MESSAGE=""
-if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
+if [ "$GITHUB_EVENT_NAME" == "schedule" ]; then
 	DATE=`date +%Y-%m-%d`
 	COMMIT_MESSAGE="Scheduled lists build: $DATE"
-elif [ "$TRAVIS_EVENT_TYPE" == "api" ]; then
+elif [ "$GITHUB_EVENT_NAME" == "workflow_dispatch" ]; then
 	DATE=`date +%Y-%m-%d`
 	COMMIT_MESSAGE="Requested lists build: $DATE"
 else
 	REV=$(git rev-parse HEAD)
-	if [ -z "$TRAVIS_REPO_SLUG" ]; then
+	if [ -z "$GITHUB_REPOSITORY" ]; then
 		COMMIT_MESSAGE="Built lists from ci/ commit: $REV"
 	else
-		COMMIT_MESSAGE="Built lists from ci/ commit: $TRAVIS_REPO_SLUG@$REV"
+		COMMIT_MESSAGE="Built lists from ci/ commit: $GITHUB_REPOSITORY@$REV"
 	fi
 fi
 
